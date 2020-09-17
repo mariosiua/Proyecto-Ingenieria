894805cbb2a3d52eccf86cb705c6903fcf71f0f8
David <davidcj31@gmail.com>
2020-09-17 15:02:08 -0600

    Introducción a bases de datos

--- ProyectoIngenieria/routes/web.php
+++ ProyectoIngenieria/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\paginasControlador;
+use App\Models\Usuario;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,27 @@ Route::get('/login', [paginasControlador::class, 'login']);
 Route::get('/contacto', [paginasControlador::class, 'contactar']);
 Route::get('/galeria', [paginasControlador::class, 'galeria']);
 
+Route::get('/leer', function(){
+    $usuario = Usuario::all();
+    foreach($usuario as $usuari){
+        echo "Nombre: " . $usuari-> usuario . "<br>";
+        echo "Contra: " . $usuari-> contrasenna . "<br>";
+        echo "Rol: " . $usuari-> rol . "<br>";
+        echo "-----------------------------". "<br>";
+
+    }
+});
+
+Route::get('/leerRol', function(){
+    $usuarios = Usuario::where([['usuario', '12345678'] , ['contrasenna','clave123']])->get();
+    return $usuarios;
+});
+
+Route::get("/insertar", function(){
+    $usuarioInser = new Usuario;
+    $usuarioInser->usuario = "9090";
+    $usuarioInser->contrasenna = "lilo";
+    $usuarioInser->rol = 1;
+
+    $usuarioInser->save();
+});
