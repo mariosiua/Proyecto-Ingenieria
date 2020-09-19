05fd9fa4187238216ef64b0f7924ce102d290fbb
David <davidcj31@gmail.com>
2020-09-19 00:33:47 -0600

    Migraciones para bases de datos (faltan)

---
+++ ProyectoIngenieria/database/migrations/2020_09_19_045945_create_personas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->primary('cedula');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->foreign('Usuario_usuario')->references('usuario')->on('usuarios');
+            $table->string('correo')->unique()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
---
+++ ProyectoIngenieria/database/migrations/2020_09_19_050749_create_asistencias_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsistenciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asistencias', function (Blueprint $table) {
+            $table->primary('codigo');
+            $table->date('clase_fecha');
+            $table->foreign('clase_hora_inicio')->references('id')-on('clases');
+            $table->foreign('Estudiante_persona_cedula')->references('Persona_cedula')-on('estudiantes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asistencias');
+    }
+}
